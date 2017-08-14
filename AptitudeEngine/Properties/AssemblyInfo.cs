f4116fb4fcf22b3133e0fcebe9a8afcc01888a2c
Tristen Horton <tristen@tristenhorton.com>
2017-08-14 02:56:19 -0700

    Added basic info and versioning for Apt

--- AptitudeEngine/Properties/AssemblyInfo.cs
+++ AptitudeEngine/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AptitudeEngine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A 2D Engine.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Phoenix Game Development ")]
 [assembly: AssemblyProduct("AptitudeEngine")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Phoenix Game Development 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2017.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
