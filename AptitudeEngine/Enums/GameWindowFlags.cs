7a8afe9e1fa3cc2ebaf784cbd23142a81c896228
Tristen Horton <tristen@tristenhorton.com>
2017-08-15 04:13:31 -0700

    another compatible type - to `GameWindowFlags` in OpenTK per #16 and #12

---
+++ AptitudeEngine/Enums/GameWindowFlags.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AptitudeEngine.Enums
+{
+    [Flags]
+    public enum GameWindowFlags
+    {
+        Default = 0,
+        Fullscreen = 1,
+        FixedWindow = 2
+    }
+}
