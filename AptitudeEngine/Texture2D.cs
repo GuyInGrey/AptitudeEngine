bcd7bb9420a1c592e1a33c784067a876293b57c8
MaliceGFS <MaliceGFS@users.noreply.github.com>
2018-01-30 17:22:11 -0600

    Misc changes to Texture2D.cs

--- AptitudeEngine/Texture2D.cs
+++ AptitudeEngine/Texture2D.cs
@@ -8,11 +8,9 @@ namespace AptitudeEngine
 {
     public struct Texture2D
     {
-        public int Width { get; }
-
-        public int Height { get; }
-
         public int ID { get; }
+        public int Width { get; }
+        public int Height { get; }
 
         public Texture2D(int id, int width, int height)
         {
@@ -44,7 +42,7 @@ namespace AptitudeEngine
             var id = GL.GenTexture();
             GL.BindTexture(TextureTarget.Texture2D, id);
 
-            var data = bmp.LockBits(new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly,
+            var data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly,
                 System.Drawing.Imaging.PixelFormat.Format32bppArgb);
             GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
                 OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
@@ -57,9 +55,9 @@ namespace AptitudeEngine
                 TextureWrapMode.Clamp);
 
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter,
-                (int) TextureMinFilter.Linear);
+                (int)TextureMinFilter.Linear);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter,
-                (int) TextureMagFilter.Linear);
+                (int)TextureMagFilter.Linear);
 
             var w = bmp.Width;
             var h = bmp.Height;
