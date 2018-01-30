ba279945231c5269ef52a1acf61831e9c101bc4b
GuyInGrey <lala.heber@gmail.com>
2018-01-29 17:06:57 -0700

    Removed Unneeded Code

--- AptitudeEngine/Components/FlairCanvas.cs
+++ AptitudeEngine/Components/FlairCanvas.cs
@@ -5,6 +5,6 @@ namespace AptitudeEngine.Components
     //https://github.com/PhoenixGameDevelopmentTeam/AptitudeEngine/issues/15
     public class FlairCanvas : AptComponent
     {
-        public override string ToString() => "Boi you found the canvas!";
+
     }
 }
