21f72aefcd5279b5b15afcf0bc9d2ba28d429e73
GuyInGrey <lala.heber@gmail.com>
2018-02-07 21:11:09 -0700

    Turtle

--- AptitudeEngine.Tests/GameContextTest.cs
+++ AptitudeEngine.Tests/GameContextTest.cs
@@ -7,6 +7,7 @@ using AptitudeEngine.Logger;
 using AptitudeEngine.Events;
 using AptitudeEngine.Enums;
 using AptitudeEngine.Components.Flairs;
+using AptitudeEngine.Components.Turtle;
 
 namespace AptitudeEngine.Tests
 {
@@ -44,52 +45,24 @@ namespace AptitudeEngine.Tests
             cameraObject.Transform.Size = new Vector2(1, 1);
             var camera = cameraObject.AddComponent<Camera>();
             camera.SetAsActive();
-            camera.SetPosition(new Vector2(0.5f, 0.5f));
+            camera.SetPosition(new Vector2(0, 0));
 
-            var parallaxManager = context.Instantiate().AddComponent<ParallaxManager>();
-            parallaxManager.Image = @"./assets/parallax_background.jpg";
+            var TurtleObject = context.Instantiate();
+            var turtle = TurtleObject.AddComponent<Turtle>();
 
-            var checkBoxObject = context.Instantiate();
-            checkBoxObject.Transform.Size = new Vector2(0.25f, 0.25f);
-            checkBoxObject.Transform.Position = new Vector2(0.05f, 0.35f);
-            var checkBox = checkBoxObject.AddComponent<CheckBox>();
+            turtle.DrawCode = t =>
+            {
+                t.SetLineThickness(2);
+                var q = 0.001f;
 
-            var buttonObject = context.Instantiate();
-            buttonObject.Transform.Size = new Vector2(0.25f, 0.25f);
-            buttonObject.Transform.Position = new Vector2(0.05f, 0.05f);
-            var button = buttonObject.AddComponent<Button>();
-            button.Click += Button_Click;
-            button.AddText("Hello\nThere!", 6f);
-
-            var sliderObject = context.Instantiate();
-            sliderObject.Transform.Size = new Vector2(0.5f, 0.05f);
-            sliderObject.Transform.Position = new Vector2(0.05f, 0.65f);
-            var slider = sliderObject.AddComponent<Slider>();
-            slider.Background = true;
-
-            var panelObject = context.Instantiate();
-            panelObject.Transform.Size = new Vector2(0.35f, 0.8f);
-            panelObject.Transform.Position = new Vector2(0.05f, 0.05f);
-            var panel = panelObject.AddComponent<Flair>();
-            panelObject.AddChild(checkBoxObject);
-            panelObject.AddChild(buttonObject);
-            panelObject.AddChild(sliderObject);
-
-            var destroyerObject = context.Instantiate();
-            var renderer = destroyerObject.AddComponent<SpriteRenderer>();
-            renderer.Sprite = Asset.Load<SpriteAsset>("./assets/starDestroyer.png");
-            destroyerObject.Transform.Size = new Vector2(1f, 1f);
-            var movement = destroyerObject.AddComponent<DestroyerMovement>();
-
-            var fireObject = context.Instantiate();
-            fireObject.Transform.Size = new Vector2(0.2f, 0.4f);
-            var fireRenderer = fireObject.AddComponent<SpriteRenderer>();
-            fireRenderer.Sprite = Asset.Load<SpriteAsset>("./assets/fire.png");
-            var fireAnimator = fireObject.AddComponent<SpriteAnimator>();
-            fireAnimator.Animation = Animation.EasyMake(8, 4, 1, 1, 30);
+                for (var i = 0; i < 100; i++)
+                {
+                    t.Move(q);
+                    t.Turn(110);
+                    q += 0.003f;
+                }
+            };
         }
-
-        private void Button_Click(object sender, MouseButtonEventArgs e) => LoggingHandler.Log("Button Clicked!", LogMessageType.Fine);
 
         public void Dispose()
         {
@@ -111,38 +84,6 @@ namespace AptitudeEngine.Tests
             context = null;
 
             disposed = true;
-        }
-    }
-
-    public class DestroyerMovement : AptComponent
-    {
-        public float Speed { get; set; } = 0.001f;
-        public float TurnSpeed { get; set; } = 0.5f;
-
-        public InputCode ForwardCode { get; set; } = InputCode.Up;
-        public InputCode LeftCode { get; set; } = InputCode.Left;
-        public InputCode RightCode { get; set; } = InputCode.Right;
-
-        public override void Render(FrameEventArgs a)
-        {
-            if (Input.GetKeyDown(ForwardCode))
-            {
-                Transform.Position = Transform.Position.Move(Transform.Rotation, Speed);
-            }
-            if (Input.GetKeyDown(RightCode))
-            {
-                Transform.Rotation += TurnSpeed;
-            }
-            if (Input.GetKeyDown(LeftCode))
-            {
-                Transform.Rotation -= TurnSpeed;
-            }
-            if (Input.GetKeyDown(InputCode.Down))
-            {
-                Transform.Position = Transform.Position.Move(Transform.Rotation, -Speed);
-            }
-
-            Context.ActiveCamera.SetPosition(Transform.Position + (Transform.Size / 2));
         }
     }
 }
---
+++ AptitudeEngine/Components/Turtle/Turtle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using AptitudeEngine.CoordinateSystem;
+using AptitudeEngine.Events;
+
+namespace AptitudeEngine.Components.Turtle
+{
+    public class Turtle : AptComponent
+    {
+        public Action<Turtle> DrawCode;
+        private Color currentColor = Color.Black;
+        private float thickness = 5;
+        private bool debug = true;
+
+        public void SetDebug(bool b) => debug = b;
+
+        public void SetColor(Color c) => currentColor = c;
+
+        public void SetLineThickness(float f) => thickness = f;
+
+        public void Jump(float distance) => Transform.Position = NewPos(distance);
+
+        public void Turn(float degrees) => Transform.Rotation += degrees;
+
+        public void Move(float distance)
+        {
+            ScreenHandler.Lines(new Vector2[] { Transform.Position, NewPos(distance) }, thickness, currentColor);
+            Transform.Position = NewPos(distance);
+        }
+
+        public void SetPosition(Vector2 v) => Transform.Position = v;
+
+        public void SetRotation(float f) => Transform.Rotation = f;
+
+        private Vector2 NewPos(float distance) => (new Vector2(Transform.Position.X, Transform.Position.Y - distance)).Rotate(Transform.Position, Transform.RotationRadians);
+
+        public override void Render(FrameEventArgs a)
+        {
+            ScreenHandler.Flags = DrawFlags.None;
+
+            thickness = 5;
+            currentColor = Color.Black;
+            Transform.Rotation = 90;
+            Transform.Position = Vector2.Zero;
+            DrawCode(this);
+
+            if (debug)
+            {
+                ScreenHandler.Circle(Vector2.Zero, 0.005f, Color.Green);
+                ScreenHandler.Circle(Transform.Position, 0.005f, Color.Red);
+            }
+        }
+    }
+}
